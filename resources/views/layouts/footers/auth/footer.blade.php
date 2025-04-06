8423c510a8b3cb01ba6b3f07a655ab32d59d9ba3
Dev-OriolCC <cesaroriolcortezz@gmail.com>
2025-04-05 23:16:14 -0500

    Added Template

---
+++ resources/views/layouts/footers/auth/footer.blade.php
@@ -0,0 +1,23 @@
+<footer class="footer pb-4">
+    <div class="container-fluid">
+        <div class="row align-items-center justify-content-lg-between">
+            <div class="col-lg-6 mb-lg-0 mb-4">
+                <div class="copyright text-center text-sm text-muted text-lg-left">
+                    © {{ now()->year }} Software by <a style="color: #252f40;" href="https://www.website.com" class="font-weight-bold ml-1"
+                        target="_blank">Company Name</a>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+                    <li class="nav-item">
+                        <a href="https://www.website.com class="nav-link text-muted" target="_blank">Company</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://www.website.com" class="nav-link text-muted"
+                            target="_blank">About Us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
